2e8cfb8b7fda35568bc82c140470689ba1f5789e
Peyton L <gabybiscuits@gmail.com>
2020-04-12 00:13:58 -0700

    First Pass at Animation

--- secret-hitler-online-interface/src/custom-alert/PolicyEnactedAlert.js
+++ secret-hitler-online-interface/src/custom-alert/PolicyEnactedAlert.js
@@ -4,17 +4,29 @@ import OptionPrompt from "./OptionPrompt";
 
 import LiberalPolicy from '../assets/policy-liberal.png';
 import FascistPolicy from '../assets/policy-fascist.png';
+import FolderCoverFront from '../assets/policy-folder-cover-front.png';
+import FolderCoverBack from '../assets/policy-folder-cover-back.png';
+import FolderBack from '../assets/policy-folder-back.png';
 
 import './PolicyEnactedAlert.css';
+import {LIBERAL} from "../GlobalDefinitions";
 
 class PolicyEnactedAlert extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            showPolicy: false
+            className: ""
         };
-        setTimeout(()=> {this.setState({showPolicy: true})}, 500);
+        this.setAnimation = this.setAnimation.bind(this);
+        this.setAnimation();
+
+    }
+
+    setAnimation() {
+        this.setState({className: ""});
+        setTimeout(()=> {this.setState({className: "show-policy-1"})}, 1000);
+        setTimeout(()=> {this.setState({className: "show-policy-2"})}, 1400);
     }
 
     render() {
@@ -24,11 +36,30 @@ class PolicyEnactedAlert extends Component {
                     return <h2>POLICY ENACTED</h2> // allows us to align it with center
                 }}
                 buttonText={"OKAY"}
+                buttonOnClick={this.setAnimation}
             >
                 <div id={"policy-enacted-container"}>
+                    <img id={"policy-enacted-cover-front"}
+                         src={FolderCoverFront}
+                         className={this.state.className}
+                         alt={"A manila folder labeled 'New Policy.'"}
+                    />
+                    <img id={"policy-enacted-cover-back"}
+                         src={FolderCoverBack}
+                         className={this.state.className}
+                         alt={"A manila folder labeled 'New Policy.'"}
+                    />
                     <img id={"policy-enacted-policy"}
-                         className={this.state.showPolicy ? "policy-alert-show" : ""}
-                         src={LiberalPolicy} alt={"A " + this.props.policyType.toLowerCase() + " policy was enacted!"}/>
+                         className={this.state.className}
+                         src={LiberalPolicy} alt={"A " + this.props.policyType.toLowerCase() + " policy that was enacted! " +
+                                                  this.props.policyType === LIBERAL ? "It's printed in blue with a dove insignia on it."
+                                                  : "It's printed in red with a skull insignia on it."}
+                    />
+                    <img id={"policy-enacted-back"}
+                         className={this.state.className}
+                         src={FolderBack}
+                         alt={""}
+                    />
                 </div>
             </OptionPrompt>
         );
