6f02438eea09abe9b02d1c76eadbc73fbd6c30e9
Peyton Lee <gabybiscuits@gmail.com>
2021-02-01 21:35:21 -0800

    Added gif images to login page

--- secret-hitler-online-interface/src/LoginPageContent.js
+++ secret-hitler-online-interface/src/LoginPageContent.js
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import ReactGA from "react-ga";
 import "./LoginPageContent.css";
+import {gifPlacePolicy, gifShowPolicy, gifShowVotes} from "./assets";
+import AliceCarousel from 'react-alice-carousel';
+import "./util/CustomAliceCarousel.css";
+
 
 class LoginPageContent extends Component {
 
@@ -24,19 +28,32 @@ class LoginPageContent extends Component {
     };
 
     render() {
+        let handleDragStart = (e) => e.preventDefault();
+        let items = [
+            <img id={"login-page-gif"} src={gifPlacePolicy} onDragStart={handleDragStart} alt={"A policy tile being placed on the board."}/>,
+            <img id={"login-page-gif"} src={gifShowPolicy} onDragStart={handleDragStart} alt={"An animated folder revealing a policy tile."}/>,
+            <img id={"login-page-gif"} src={gifShowVotes} onDragStart={handleDragStart} alt={"An animation showing all the cast votes."}/>
+        ];
         return (
             <>
-                <div id={"login-page-description-container"}>
-                    <h2>What is Secret Hitler Online?</h2>
-
+                <div id={"#login-page-description-container"}>
                     <div id={"login-page-description-text-container"}>
+                        <h2 id={"login-page-description-text-header"}>What is Secret Hitler Online?</h2>
                         <p id={"login-page-description-text"}>
                             Secret Hitler Online is an adaptation of the original Secret Hitler board game,
                             reimagined for the web.
                             It supports 5-10 players, featuring smooth art and animations with all the secrecy and
                             intrigue of the original. It's designed to be easy to pick up and play for any quarantine
-                            game night. Play for free in your browser, with no ads ever!
-                            <br/><br/>
+                            game night.<br/><br/>Play for free in your browser, with no ads ever!
+                            <br/> <br/>
+                        </p>
+                    </div>
+                    <div id={"login-page-gif-container"}>
+                        {items}
+                    </div>
+                    <div id={"login-page-description-text-container"}>
+                        <p id={"login-page-description-text"}>
+                            <br/>
                             The project is open-source, and is licensed under <a
                                 href={"https://creativecommons.org/licenses/by-nc-sa/4.0/"}
                                 rel="noopener"
@@ -54,17 +71,24 @@ class LoginPageContent extends Component {
                                 Secret Hitler
                             </a> board game by Goat, Wolf, & Cabbage (© 2016-2020). Developed by ShrimpCryptid (© 2020-2021).
                             <br/><br/>
-                            Found an bug or want to leave a comment? Report bugs on the <a href={"https://github.com/ShrimpCryptid/Secret-Hitler-Online/issues"}
+                            Found a bug or want to leave a comment? Report bugs on the <a href={"https://github.com/ShrimpCryptid/Secret-Hitler-Online/issues"}
                                                                                              rel="noopener"
                                                                                              target={"_blank"}>Issues page</a>.
                         </p>
                     </div>
+
                 </div>
             </>
         );
     }
 
 }
+
+/*
+<div id={"login-page-carousel-container"}>
+                        <AliceCarousel mouseTracking items={items} />
+                    </div>
+ */
 
 LoginPageContent.propTypes = {
 };
